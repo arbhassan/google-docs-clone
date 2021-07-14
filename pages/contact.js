012506347a2ea6b0369d564b64f7d2d633352436
arbhassan <hassanjawad1321@gmail.com>
2021-07-14 18:48:00 +0500

    Updated the README.md page

--- pages/contact.js
+++ pages/contact.js
@@ -1,10 +1,11 @@
 import Head from "next/head";
 
-export default function Home() {
+export default function Contact() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <p>CONTACT ME NIGGA</p>
       <Head>
-        <title>Create Next App</title>
+        <title>Contact Me</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
     </div>
